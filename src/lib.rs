dac6b403f228556d3b59f9dde52802f04f28a6e6
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-16 22:37:07 +0800

    feat: encode the header and payload as a message

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,21 @@ pub struct Header {
     /// Number of seconds since the UNIX epoch.
     timestamp: u64,
 }
+
+#[derive(Decode, Encode)]
+pub enum Payload {
+    /// This message informs the Cloud server that the
+    /// metro reading conflicts with that of the tap.
+    Conflict,
+    /// This message is a ping from the unit on the detected flow rate.
+    Flow {
+        ticks: u64,
+    },
+}
+
+/// A unit of message sent to the Cloud server.
+#[derive(Decode, Encode)]
+pub struct Message {
+    head: Header,
+    data: Payload,
+}

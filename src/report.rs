975629f8b011f907871774c45cef0673e87236fc
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-21 01:32:27 +0800

    fix(report): use public fields

--- src/report.rs
+++ src/report.rs
@@ -6,18 +6,19 @@ pub type MacAddress = [u8; 6];
 /// Generic ping when there is no data to be sent. Because it sends no data
 /// in the payload, this is effectively the common header that is used by all
 /// messages in the protocol. This may occur during leak announcements.
-#[derive(Debug, Decode, Encode)]
+#[derive(Debug, Decode, Encode, PartialEq, Eq)]
 pub struct Ping {
     /// MAC address of the packet as a (sufficently) unique ID.
-    mac: MacAddress,
+    pub mac: MacAddress,
 }
 
 /// This message is a ping from the unit on the detected flow rate.
-#[derive(Debug, Decode, Encode)]
+#[derive(Debug, Decode, Encode, PartialEq, Eq)]
 pub struct Flow {
     /// Common header is the ping message.
-    head: Ping,
+    pub head: Ping,
     /// Detected flow rate measured in revolutions per second
     /// (according to the water flow sensor).
-    flow: u64,
+    #[bitcode_hint(expected_range = "0..16384")]
+    pub flow: u32,
 }

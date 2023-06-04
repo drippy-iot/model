7354a4a470d79e15370563862d93a39b57fde346
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-06-04 14:52:12 +0800

    feat!: merge leak detection and flow reporting

--- src/report.rs
+++ src/report.rs
@@ -2,10 +2,12 @@ use bitcode::{Decode, Encode};
 
 /// This message is a ping from the unit on the detected flow rate.
 #[derive(Debug, Decode, Encode, PartialEq, Eq)]
-pub struct Flow {
+pub struct Ping {
     /// Common header is the ping message.
     pub addr: crate::MacAddress,
     /// Detected flow rate measured in revolutions per second
     /// (according to the water flow sensor).
     pub flow: u16,
+    /// Checks whether a leak was detected since the last ping.
+    pub leak: bool,
 }
